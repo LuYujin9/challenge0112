2e0db9b670a7ead0ac112b69d35b0403c0d88f4f
Andreas <12122390+andrewgreenh@users.noreply.github.com>
2023-12-06 18:08:21 +0100

    Adds todos and notes

--- src/date.type.tsx
+++ src/date.type.tsx
@@ -1,5 +1,11 @@
 export type Country = {
   id: string;
   name: string;
+  /**
+   * TODO: please move this out of the country objects.
+   * If the list is fetched from a backend, you will always receive them without this field
+   * and thus keeping this state outside of the data objects makes it a lot easier
+   * to deal with updates, refetches, etc.
+   */
   isSelected: boolean;
 };
